e911a972a04f0519650ec69b21ae771bebca44a6
jiminkim92 <wlalsl48@naver.com>
2023-03-19 09:59:35 +0900

    2023.03.18. java

---
+++ java01/src/variable/Type2.java
@@ -0,0 +1,24 @@
+package variable;
+
+public class Type2 {
+	public static void main(String[] args) {
+		// 변수를 활용해서
+		// 이름, 출생년도, 키
+		
+		// 예시의 ooo을 변수로 사용		
+		/*
+		 이름 : ooo
+		 출생년도 : oooo년
+		 키 : ooo.oocm
+		*/
+		
+		String name = "홀길동";
+		int birth = 2000;
+		double height = 175.5;
+						
+		System.out.println("이름 : "+name);
+		System.out.println("출생년도 : "+birth);
+		System.out.println("키 : "+height+"cm");
+		
+	}
+}
