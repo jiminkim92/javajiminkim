7259cfe90dd2ace7cc298a44ba34fd995ffaf91d
hyuk0u0 <hyuk0u0@naver.com>
2023-05-13 13:24:03 +0900

    2023.05.13java

--- java14/src/iostream/MySocketClient.java
+++ java14/src/iostream/MySocketClient.java
@@ -12,7 +12,8 @@ import java.util.Scanner;
 public class MySocketClient {
 	public static void main(String[] args) {
 		// 채팅 프로그램
-		// 네트워크를 통해 메시지를 주고 받을 땐 Soket 을 사용
+		// 네트워크를 통해 메시지를 주고 받을 땐 Socket 을 사용
+		// IP 와 Port (IP : 해당 컴퓨터의 주소, Port : 프로그램의 식별코드)
 		// 클라이언트1 -- 서버 -- 클라이언트2
 		MyClient user = new MyClient();
 		user.start();
@@ -21,10 +22,10 @@ public class MySocketClient {
 
 class MyClient extends Thread{
 	Socket socket;
-	InputStream is;			// 서버의 메시지 받을 때
-	OutputStream os;		// 서버로 메시지 보낼 때
-	ObjectInputStream ois;
-	ObjectOutputStream oos;
+	InputStream is;				// 서버의 메시지 받을 때
+	OutputStream os;			// 서버로 메시지 보낼 때
+	ObjectInputStream ois;		// 매핑
+	ObjectOutputStream oos;		// 매핑
 	
 	// 임시 입력용 Scanner
 	Scanner sc = new Scanner(System.in);
@@ -32,13 +33,16 @@ class MyClient extends Thread{
 	@Override
 	public void run() {
 		// 해당 컴퓨터와 연결 (IP, port)
+		// port : 해당 프로그램의 포트
+		// IP : cmd -> ipconfig
 		try {
 			// localhost : 자기자신의 컴퓨터 == "127.0.0.1"
-			socket = new Socket("192.168.3.101", 8089);		// 소켓을 생성(이제부터 통신 가능)
+			//
+			socket = new Socket("127.0.0.1", 8089);			// 소켓을 생성(이제부터 통신 가능)
 			os = socket.getOutputStream();
 			is = socket.getInputStream();
-			oos = new ObjectOutputStream(os);			// 보낼 준비 완료
-			ois = new ObjectInputStream(is);			// 받을 준비 완료
+			oos = new ObjectOutputStream(os);		// 보낼 준비 완료
+			ois = new ObjectInputStream(is);		// 받은 준비 완료
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -55,21 +59,23 @@ class MyClient extends Thread{
 	// 서버로 메시지를 보내는 함수
 	public void sendMessage() {
 		System.out.println("보낼 메시지:");
-		String msg = sc.nextLine();	
+		String msg = "";
 		try {
+			if(sc.hasNextLine()) {
+				msg = sc.nextLine();
+			}
 			oos.writeObject(msg);
-			oos.flush();				// 버그방지 (보내고나서 쓰레기값이 남아있을 수도 있기 때문에 비움)
-		} catch (IOException e) {
+			oos.flush();		// 버그방지 (보내고나서 쓰레기값이 남아있을 수도 있기 때문에 비움)
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-			
 	}
 	
 	// 서버가 보내온 메시지를 받는 함수
 	public void recvMessage() {
 		String msg = "";
 		try {
-			msg = (String) ois.readObject();		// 서버로부터 메시지를 받는다
+			msg = (String)ois.readObject();			// 서버로부터 메시지를 받는다
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -77,5 +83,6 @@ class MyClient extends Thread{
 		}			
 		
 		System.out.println(msg);		// 임시용 출력
-	}	
+	}
+	
 }
